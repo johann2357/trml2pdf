51cb6ce1a9c7778c03d1ca019e2174ce13dcf582
johann2357 <johann2357@gmail.com>
2014-05-27 12:10:15 -0500

    pip setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='trml2pdf',
+    version='1.0',
+    description='RML TO PDF',
+    author='romanlv',
+    url='https://github.com/johann2357/trml2pdf',
+    packages=['trml2pdf', ],
+)
