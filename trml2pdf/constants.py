3bcc20340d1054c6fb396ed0ce17d2667fe05167
johann2357 <johann2357@gmail.com>
2014-07-04 15:40:32 -0500

    generalizing for django

--- trml2pdf/constants.py
+++ trml2pdf/constants.py
@@ -1,1 +1,8 @@
-fonts_location = '../../../../path/to/fonts/'
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+fonts_location = getattr(settings, 'FONTS_DIR', None)
+if not fonts_location:
+    raise ImproperlyConfigured(
+        'There is no FONTS_DIR variable in your settings.py'
+    )
